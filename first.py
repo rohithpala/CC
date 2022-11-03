de52bb8b93972fc313b9205d2fa02e095e4d1f17
Rohith Pala <rohithpala02@gmail.com>
2022-11-03 15:53:00 +0530

    .

--- first.py
+++ first.py
@@ -1,45 +1,17 @@
-def first(string):
-    first_ = []
-    if string in non_terminals:
-        alts = productions_dict[string]
-        for alt in alts:
-            first1 = first(alt[0])
-            if first1 not in first_:
-                first_.extend(first1)
-    elif string in terminals or string == "@":
-        if string not in first_:
-            first_.extend(string)
-            return string
-    return first_
+from collections import defaultdict
 
-noOfTerminals = int(input("Enter no. of terminals:"))
-terminals = []
-print("Enter Terminals:")
-for _ in range(noOfTerminals):
-    terminals.append(input())
-noOfNonTerminals = int(input("Enter no. of non terminals:"))
-non_terminals = []
-print("Enter Non Terminals:")
-for _ in range(noOfNonTerminals):
-    non_terminals.append(input())
-productions_dict = dict()
-for nt in non_terminals:
-    productions_dict[nt] = []
-productions_dict = dict()
-for nt in non_terminals:
-    productions_dict[nt] = []
-print("Enter Productions:")
-for _ in range(len(non_terminals)):
-    lhs, rhs = input().split("->")
-    alts = rhs.split("/")
-    for alt in alts:
-        productions_dict[lhs].append(alt)
-
-first_dict = dict()
-for nt in non_terminals:
-    first_dict[nt] = set(first(nt))
-for k, v in first_dict.items():
-    print("FIRST(" + k + ") : ", end="{ ")
-    for i in v:
-        print(i, end=" ")
-    print("}")
+First = defaultdict(list)
+def first(productionSet,nt,parent):
+   
+    for x in productionSet[nt]:
+        if not x[0].isupper():
+            if x[0] not in First[parent]:
+                First[parent].append(x[0])
+        else:
+            first(productionSet,x[0],parent)
+productionSet = {'E' : ['TZ'],'Z' : ['+TZ','e'],'T':['FY'],'Y': ['*FY','e'],'F':['(E)','i']}
+for key,val in productionSet.items():
+    first(productionSet,key,key)
+   
+for key,val in First.items():
+    print("First({0}) = {1}".format(key,val))
